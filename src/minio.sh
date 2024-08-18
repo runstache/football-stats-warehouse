f85cae853fa54b3b7c2a11d2e43354fbfc926161
Larry Shields <larry.shields@gmail.com>
2024-08-18 14:14:09 -0400

    Updated minio script

--- src/minio.sh
+++ src/minio.sh
@@ -5,11 +5,11 @@ bucket=$2
 
 types=(games players teams)
 
-for str in ${types[@]}; do
+for str in "${types[@]}"; do
   echo "Staging $str information..."
-  mc cp --recursive "$root_folder/games/year=$str/type=1/"  myminio/$bucket/$str/$3/preseason/
-  mc cp --recursive "$root_folder/games/year=$str/type=2/"  myminio/$bucket/$str/$3/regular/
-  mc cp --recursive "$root_folder/games/year=$str/type=3/"  myminio/$bucket/$str/$3/postseason/
+  mc cp --recursive "$root_folder/games/year=$str/type=1/"  "myminio/$bucket/$str/$3/preseason/"
+  mc cp --recursive "$root_folder/games/year=$str/type=2/"  "myminio/$bucket/$str/$3/regular/"
+  mc cp --recursive "$root_folder/games/year=$str/type=3/"  "myminio/$bucket/$str/$3/postseason/"
 done
 
 echo "Done"
